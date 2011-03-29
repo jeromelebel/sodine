34353dea1349cbae8a521f350f5c8f4556320d08
Jérôme Lebel <jerome@fotonauts.com>
2011-03-30 01:46:38 +0200

    adding iodine into the iphone app

--- test-iodined/main.m
+++ test-iodined/main.m
@@ -8,15 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
+int iodine_main(int argc, char **argv);
+
 int main (int argc, const char * argv[])
 {
-
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
-
-    [pool drain];
-    return 0;
+    return iodine_main(argc, argv);
 }
 
