4ffb4dbb55c4f5858692d809107296e29167ea22
Jérôme Lebel <jerome@fotonauts.com>
2011-03-30 00:37:23 +0200

    some changes

---
+++ test-iodined/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test-iodined
+//
+//  Created by Jérôme Lebel on 29/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
